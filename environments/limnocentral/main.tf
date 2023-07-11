6d7fda98f181730a3e2b56b04f4fbc41d5478a18
Kingdon Barrett <kingdon@weave.works>
2023-07-11 10:19:09 -0400

    testing terraform provider @ 1.0.1/Flux v2.0.1
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- environments/limnocentral/main.tf
+++ environments/limnocentral/main.tf
@@ -16,7 +16,7 @@ terraform {
   required_providers {
     flux = {
       source = "registry.terraform.io/fluxcd/flux"
-      version = "1.0.0-rc.5"
+      version = "1.0.1"
     }
   }
 }
@@ -27,5 +27,5 @@ resource "flux_bootstrap_git" "this" {
     "image-reflector-controller",
     "image-automation-controller"
   ]
-  # version = "v2.0.0-rc.5"
+  # version = "v2.0.1"
 }
